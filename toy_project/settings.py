d7b6d2b4a686fdbfd81e397ed11958a5a6077e8d
Taegyu An <59655047+Ahn-droid@users.noreply.github.com>
2023-12-03 16:44:09 +0900

    modify STATIC_ROOT

--- toy_project/settings.py
+++ toy_project/settings.py
@@ -131,14 +131,14 @@ STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATIC_ROOT = os.path.join(BASE_DIR, '_static')
+"""
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-
+"""
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
 
